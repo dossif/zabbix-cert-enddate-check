dd038d7ea64359afd0eda3266af308485a4f6f9b
dossif <d.ossif@gmail.com>
2022-07-04 11:44:07 +0300

    Update cert_enddate_check.sh

--- cert_enddate_check.sh
+++ cert_enddate_check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ -z "$1" ]; then echo "Example: $0 www.google.com"; exit 1; fi
-domain=$1
-cert_date=$(echo | openssl s_client -servername $domain -connect $domain:443 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f 2)
+if [ -z "$1" ]; then echo "Example: $0 google.com:443"; exit 1; fi
+host=$1
+domain=$(echo $host | perl -ne '/(.*):/ && print $1')
+cert_date=$(echo | openssl s_client -servername $domain -connect $host 2>/dev/null | openssl x509 -noout -enddate | cut -d= -f 2)
 cert_timestamp=$(date -d "$cert_date" "+%s")
 now_timestamp=$(date -d now "+%s")
 diff_in_days=$(((cert_timestamp-now_timestamp)/86400))
